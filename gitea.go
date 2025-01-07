ff5666d7ea0077cb8e00eaa592e6f438c31d24c0
ebner <simon.ebner@psi.ch>
2025-01-07 12:56:58 +0100

    change name

--- gitea.go
+++ gitea.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/42wim/caddy-gitea/pkg/gitea"
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/simongregorebner/caddy-gitea/pkg/gitea"
 )
 
 func init() {
